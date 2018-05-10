6010228310cb062ff0dad4a8977ca24fcf2a1b16
Francois Marais <francois.m@superbalist.com>
2018-05-10 21:06:39 +0200

    add cat dropdown to add post

--- admin/posts/add_post.php
+++ admin/posts/add_post.php
@@ -5,8 +5,18 @@
     </div>
 
     <div class="form-group">
-        <label for="post_category_id">Post Category Id</label>
-        <input type="text" class="form-control" name="post_category_id">
+        <select name="post_category" id="post_category">
+            <?php
+
+            // get categories
+            $query_categories = "SELECT * FROM categories";
+            $query_response_categories = mysqli_query($connection, $query_categories);
+
+            while ($row = mysqli_fetch_assoc($query_response_categories)) {
+                echo "<option value='{$row['cat_id']}'>{$row['cat_title']}</option>";
+            }
+            ?>
+        </select>
     </div>
 
     <div class="form-group">
